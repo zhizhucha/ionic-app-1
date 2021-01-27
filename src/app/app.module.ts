b96cd9b2bee2acfbeac3eaee299f654f9ddaac3c
daoliangshu <quentin.cartier.dev@gmail.com>
2021-01-27 16:31:02 +0100

    Add firebase to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Import the AngularFire Module
+import { AngularFireModule } from '@angular/fire';
+//import { AngularFireAuthModule } from '@angular/fire/auth';
+import{ AngularFireAnalyticsModule} from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+            AppRoutingModule,
+            AngularFireModule.initializeApp(environment.firebase),
+           AngularFireAnalyticsModule,
+          AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
