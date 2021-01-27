e024ca864f7a146f0201d3732be5a1d7de0e06e2
Emeziem U <emez.uwalaka@gmail.com>
2021-01-27 17:48:31 +0100

    Removed environment files. Added new package-lock.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // Import the AngularFire Module
 import { AngularFireModule } from '@angular/fire';
-//import { AngularFireAuthModule } from '@angular/fire/auth';
-import{ AngularFireAnalyticsModule} from '@angular/fire/analytics';
+// import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAnalyticsModule} from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), 
+  imports: [BrowserModule, IonicModule.forRoot(),
             AppRoutingModule,
             AngularFireModule.initializeApp(environment.firebase),
            AngularFireAnalyticsModule,
