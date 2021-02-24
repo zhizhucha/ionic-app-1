513ae90aae771b501d20b2987b775c161a6552b6
daoliangshu <quentin.cartier.dev@gmail.com>
2021-02-24 15:01:18 +0100

    correction navigation tab1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from './services/user/auth.guard';
 const routes: Routes = [
   {
     path: 'lists',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule),
     canActivate: [AuthGuard],
   },
   {
