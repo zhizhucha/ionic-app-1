8293e59b951f30c976baed134d133ed804720a44
daoliangshu <quentin.cartier.dev@gmail.com>
2021-02-24 11:04:26 +0100

    Add verification email sending

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import {AngularFireAuth} from '@angular/fire/auth';
+import User from "../../../node_modules/firebase"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth) { }
+  user : User.User;
 
-  doLogin(value) {
+
+  constructor(public afAuth: AngularFireAuth) { 
+
+    // Subscribe to the authentification state
+    this.afAuth.authState.subscribe(user => {
+      if(user){
+        this.user = user;
+        localStorage.setItem('user', JSON.stringify(this.user));
+      } else {
+        localStorage.setItem('user', null);
+      }
+    })
+
+
+  }
+
+  doLogin(value : any) {
     console.log(value);
 
     return new Promise<any>((resolve, reject) => {
@@ -24,16 +41,26 @@ export class AuthService {
 
 
 
-  doRegister(mform) {
+  doRegister(mform : any) {
     console.log(mform);
 
     return new Promise<any>((resolve, reject) => {
       this.afAuth.createUserWithEmailAndPassword(mform.email, mform.password)
           .then(
-              res =>  resolve(res),
+              res =>  {
+                this.sendEmailVerification();
+                resolve(res);
+              },
               err => reject(err)
           );
     });
   }
+
+
+  async sendEmailVerification(){
+    (await this.afAuth.currentUser).sendEmailVerification()
+
+  }
+
 
 }
