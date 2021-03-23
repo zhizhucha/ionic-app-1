5dacb3a4b8216693cc9927954beb6acb745bcda3
Emeziem U <emez.uwalaka@gmail.com>
2021-03-23 13:33:39 +0100

    Form for adding maximum of 3 users to list done.

--- src/app/components/add-users-to-list/add-users-to-list.component.ts
+++ src/app/components/add-users-to-list/add-users-to-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FirebaseAdminService} from '../../services/admin/firebase-admin.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-users-to-list',
@@ -7,21 +8,27 @@ import {FirebaseAdminService} from '../../services/admin/firebase-admin.service'
   styleUrls: ['./add-users-to-list.component.scss'],
 })
 export class AddUsersToListComponent implements OnInit {
-  addUsersFormGroup = [
-    {val: 'Gaia'}, {val: 'Hermes'}, {val: 'Odin'}
-  ];
+  addUsersFormGroup: FormGroup;
+  user1Email: FormControl;
+  user2Email: FormControl;
+  user3Email: FormControl;
 
-  constructor(private firebaseAdmin: FirebaseAdminService) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
-    console.log(this.firebaseAdmin.getUsers());
+    this.user1Email = new FormControl('', [Validators.email, Validators.required]);
+    this.user2Email = new FormControl('', [Validators.email]);
+    this.user3Email = new FormControl('', [Validators.email]);
+    this.addUsersFormGroup = new FormGroup({
+      user1Email: this.user1Email, user2Email: this.user2Email, user3Email: this.user3Email
+    });
   }
 
   addUsers() {
-
+    console.log('add')
   }
 
-  backToLists() {
-
+  backToHome() {
+    this.router.navigate(['home']);
   }
 }
